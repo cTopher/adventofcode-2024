b06a5042c1a2f2c569f9302d5738b1ec56ab7030
Christopher <leyssen@gmail.com>
2024-12-24 13:31:42 +0100

    Remove regex

--- day_03/src/computer.rs
+++ day_03/src/computer.rs
@@ -1,44 +1,43 @@
-use regex::Regex;
 use std::str::FromStr;
 
 pub struct Computer {
     memory: String,
-    mul_regex: Regex,
 }
 
 impl Computer {
-    fn new(memory: String) -> Self {
-        let mul_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-        Self { memory, mul_regex }
-    }
-
     pub fn calculate_sum_of_multiplications(&self) -> u32 {
-        self.sum_of_multiplications(&self.memory)
+        sum_of_multiplications(&self.memory)
     }
-
     pub fn calculate(&self) -> u32 {
         self.memory
             .split("do()")
-            .map(|s| self.sum_of_multiplications(s.split("don't()").next().unwrap()))
+            .map(|s| sum_of_multiplications(s.split("don't()").next().unwrap()))
             .sum()
     }
+}
 
-    fn sum_of_multiplications(&self, s: &str) -> u32 {
-        self.mul_regex
-            .captures_iter(s)
-            .map(|caps| {
-                let a: u32 = caps[1].parse().unwrap();
-                let b: u32 = caps[2].parse().unwrap();
-                a * b
-            })
-            .sum()
-    }
+fn sum_of_multiplications(s: &str) -> u32 {
+    s.match_indices("mul(")
+        .filter_map(|(index, _)| {
+            let start = index + 4;
+            let len = s[start..].find(')')?;
+            let mut parts = s[start..start + len].split(',');
+            let a: u32 = parts.next()?.parse().ok()?;
+            let b: u32 = parts.next()?.parse().ok()?;
+            if parts.next().is_some() {
+                None
+            } else {
+                Some(a * b)
+            }
+        })
+        .sum()
 }
 
 impl FromStr for Computer {
     type Err = !;
 
     fn from_str(s: &str) -> Result<Self, !> {
-        Ok(Self::new(s.to_string()))
+        let memory = s.to_string();
+        Ok(Self { memory })
     }
 }
